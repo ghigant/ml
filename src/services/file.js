4e49c1e2599e39bf405070957ab81219db8e482c
Valentin Botoc <ghigant@gmail.com>
2017-01-05 22:28:33 +0200

    Updates

--- src/services/file.js
+++
@@ -1,8 +0,0 @@
-import {concat} from 'lodash';
-
-export const parse = (content) => {
-  const lines = content.split(/[\r\n]+/g);
-  return lines.map((line) => {
-    return concat(...line.split(' ')).map(item => parseFloat(item, 10));
-  });
-}
